3ee9c37d9e34789cc0f9afd380bbeec9a908153a
Ziyad Hammad <ziyadhammad0@gmail.com>
2024-06-09 01:13:29 -0400

    created pages

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import Link from 'react-router-dom'
+
+const ProductCard = ({product}) => {
+  return (
+    <Link href={`/products/${product._id}`} className="sm:w-[292px] sm:max-w-[292px] w-full flex-1 flex flex-col gap-4 rounded-md">
+      <div className="flex-1 relative flex flex-col gap-5 p-4 rounded-md">
+      <img src={product.image} alt={product.title} className="max-h-[250px] object-contain w-full h-full bg-transparent" />
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h3 className="text-primary text-xl leading-6 font-semibold truncate">{product.title}</h3>
+
+        <div className="flex justify-between">
+          <p className="text-body opacity-50 text-lg capitalize">
+            {product.category}
+          </p>
+          <p className="text-primary text-lg font-semibold">
+            <span>{product?.currency} </span>
+            <span>{product?.currentPrice}</span>
+          </p>
+        </div>
+      </div>
+
+    </Link>
+  );
+};
+
+export default ProductCard;
--- frontend/src/pages/MyProducts.jsx
+++ frontend/src/pages/MyProducts.jsx
@@ -3,6 +3,17 @@ import React from 'react'
 const MyProducts = () => {
   return (
     <div>MyProducts</div>
+
+    // <section id='Trending' className="flex flex-col gap-10 px-6 md:px-20 py-24">
+    //     <h2  className=" text-primary  text-[32px] font-semibold text-center lg:text-start">Trending</h2>
+
+    //     <div className="flex flex-wrap gap-x-8 gap-y-16 justify-center md:justify-start">
+
+    //       {products.map((product) => (
+    //         <ProductCard key={product._id} product={product} />
+    //       ))}
+    //     </div>
+    //   </section> 
   )
 }
 
